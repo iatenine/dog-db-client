f1ac97310a0c3a0e37a4157cee3bdaef4957b628
Tanman <tknielsen32@gmail.com>
2022-02-17 09:09:34 -0600

    Adding new fields to login js.

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,17 +1,28 @@
+const username = document.getElementById("username");
 const email = document.getElementById("email");
-const password = document.getElementById("password");
+const password = document.getElementById("password");;
 const button = document.getElementById("button");
 
+const enterName = document.getElementById("enterName");
+const enterUsername = document.getElementById("enterUsername");
 const enterEmail = document.getElementById("enterEmail");
-const enterPassword = document.getElementById("enterPassword");
+const enterPhone = document.getElementById("enterPhoneNumber");
+const enterPassword = document.getElementById("enterPassword ");
 const button2 = document.getElementById("button2");
+//name, username, email, phone, password
+
+
 
 button.addEventListener("click", () => {
+    console.log(username.value);
     console.log(email.value);
     console.log(password.value);
     });
 
 button2.addEventListener("click", () => {
+    console.log(enterName.value);
+    console.log(enterUsername.value);
     console.log(enterEmail.value);
+    console.log(enterPhoneNumber.value);
     console.log(enterPassword.value);
     });
