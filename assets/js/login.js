e10073e4940d2144e3ff23f31b5bda0e9033e020
Jack <iatenine@gmail.com>
2022-02-18 19:01:39 -0700

    registration implemented

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,53 +1,69 @@
 const usernameInput = document.getElementById("username");
 const email = document.getElementById("email");
-const passwordInput = document.getElementById("password");;
+const passwordInput = document.getElementById("password");
 const loginButton = document.getElementById("loginButton");
 
 const enterName = document.getElementById("enterName");
 const enterUsername = document.getElementById("enterUsername");
 const enterEmail = document.getElementById("enterEmail");
 const enterPhone = document.getElementById("enterPhoneNumber");
-const enterPassword = document.getElementById("enterPassword ");
-const button2 = document.getElementById("button2");
+const enterPassword = document.getElementById("enterPassword");
+const registerButton = document.getElementById("button2");
 //name, username, email, phone, password
 
-
-
 loginButton.addEventListener("click", async () => {
-    console.log(usernameInput.value);
-    console.log(email.value);
-    console.log(passwordInput.value);
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+  var raw = JSON.stringify({
+    username: "JWTTest",
+    password: "password",
+  });
 
-var raw = JSON.stringify({
-  "username": "JWTTest",
-  "password": "password"
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+  try {
+    const response = await fetch("http://localhost:8080/login", requestOptions);
+    const result = await response.json();
+    const token = result.token;
+    if (token) localStorage.setItem("token", JSON.stringify(token));
+  } catch (error) {
+    console.log("error", error);
+  }
 });
 
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
-try{
-  const response = await fetch("http://localhost:8080/login", requestOptions);
-  const result = await response.json();
-  const token = result.token;
-  console.log(token);
-  localStorage.setItem("token",JSON.stringify(token));
-}catch(error){
-    console.log('error', error);
-}
+registerButton.addEventListener("click", async () => {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-    });
+  var raw = JSON.stringify({
+    email: enterEmail.value || "",
+    phone: enterPhoneNumber.value || "",
+    legalName: enterName.value || "",
+    username: enterUsername.value,
+    password: enterPassword.value,
+  });
 
-button2.addEventListener("click", () => {
-    console.log(enterName.value);
-    console.log(enterUsername.value);
-    console.log(enterEmail.value);
-    console.log(enterPhoneNumber.value);
-    console.log(enterPassword.value);
-    });
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  try {
+    const response = await fetch(
+      "http://localhost:8080/register",
+      requestOptions
+    );
+    const result = await response.json();
+    const token = result.token;
+    if (token) localStorage.setItem("token", JSON.stringify(token));
+  } catch (error) {
+    console.log("error", error);
+  }
+});
