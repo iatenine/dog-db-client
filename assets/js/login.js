b126dd09645c1874e6d15830d535e6629e272e39
Tanman <tknielsen32@gmail.com>
2022-02-18 19:36:06 -0600

    milestone. Authentication completed.

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,7 +1,7 @@
-const username = document.getElementById("username");
+const usernameInput = document.getElementById("username");
 const email = document.getElementById("email");
-const password = document.getElementById("password");;
-const button = document.getElementById("button");
+const passwordInput = document.getElementById("password");;
+const loginButton = document.getElementById("loginButton");
 
 const enterName = document.getElementById("enterName");
 const enterUsername = document.getElementById("enterUsername");
@@ -13,10 +13,35 @@ const button2 = document.getElementById("button2");
 
 
 
-button.addEventListener("click", () => {
-    console.log(username.value);
+loginButton.addEventListener("click", async () => {
+    console.log(usernameInput.value);
     console.log(email.value);
-    console.log(password.value);
+    console.log(passwordInput.value);
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "username": "JWTTest",
+  "password": "password"
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+try{
+  const response = await fetch("http://localhost:8080/login", requestOptions);
+  const result = await response.json();
+  const token = result.token;
+  console.log(token);
+  localStorage.setItem("token",JSON.stringify(token));
+}catch(error){
+    console.log('error', error);
+}
+
     });
 
 button2.addEventListener("click", () => {
